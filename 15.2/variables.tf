ba2fdfd063d9291bb6deb41afa45c18b928cca06
stas <superstas91@gmail.com>
2025-08-09 12:37:46 +0000

    update 15.2

---
+++ 15.2/variables.tf
@@ -0,0 +1,86 @@
+###cloud vars
+
+variable "cloud_id" {
+  type        = string
+  default     = "b1g8ta6qu7na0ir2khnv"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1g8kve3609ag8bp327e"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "zone" {
+  type = object({
+    zone-a = string
+    zone-b = string
+  })
+  default = {
+    zone-a = "ru-central1-a"
+    zone-b = "ru-central1-b"
+  }
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "cidr" {
+  type = object({
+    cidr0 = list(string)
+    cidr1 = list(string)
+  })
+  default     = {
+    cidr0 = ["192.168.10.0/24"]
+    cidr1 = ["192.168.20.0/24"]
+  }
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type = object({
+    net      = string
+    public   = string
+    private  = string
+  })
+  default = {
+    net     = "vpc"
+    public  = "public"
+    private = "private"
+  }
+  description = "VPC network & subnet name"
+}
+
+variable "image_family" {
+  type        = string
+  default     = "ubuntu-2004-lts"
+  description = "VM image family name"
+}
+
+variable "platform_id" {
+  type        = string
+  default     = "standard-v3"
+  description = "VM platform id"
+}
+
+variable "user_vm" {
+  description = "User name for VM and some values for hardcoded data"
+  type        = string
+  default     = "stas"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "SSH public key for VMs"
+}
+
+variable "service_account_id" {
+  type        = string
+  description = "Service account ID"
+}
+
+variable "bucket_name" {
+  default = "test-bucket1423090825"
+}
+
+variable "object_key" {
+  default = "test-picture"
+}
